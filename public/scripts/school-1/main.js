86395c7d146f606f146855b3a257cf36b57a9c0f
angel <perdonholaperdon123@gmail.com>
2022-08-01 17:18:12 -0400

    Stylizing the web

--- public/scripts/school-1/main.js
+++ public/scripts/school-1/main.js
@@ -4,13 +4,23 @@ const students = document.querySelector('.students');
 window.addEventListener('hashchange', () => {
     console.log(window.location.hash);
 });
+
+const fragment = document.createDocumentFragment();
+const divElement = document.createElement('DIV');
+divElement.className = "Nuevo"
 studentsTest.map(student => {
-    students.innerHTML += `
+    const card = `
     <div class="card" style = 'background: #fefefe'>
         <div class="card-body">
             <h4 class="card-title">${student}</h4>
             <p class="card-text">School:Next Bootcamp</p>
+            <p id ='etiqueta'>See project</p> 
+            <button id ='boton'></button>
         </div>
     </div>`
+    divElement.innerHTML += card;
+    fragment.appendChild(divElement); 
 }) 
+
+students.appendChild(fragment);
 
