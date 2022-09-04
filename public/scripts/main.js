115974ceab50805e0c91e8803e6ed6ab50ccb1b7
Yefri Encarnación <yefriej128@gmail.com>
2022-09-03 22:46:39 -0400

    fixing main view get students bug

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,49 +1,50 @@
 const students = document.querySelector('.students');
 const login = document.getElementById("login");
-login.innerHTML = "logout";
+login.innerHTML = "logout"; // TODO: ponerlo directamente en el html.
+
 login.addEventListener("click", () => {
   document.cookie = `jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
-  console.log(document.cookie);
   localStorage.removeItem("jwt");
 })
 
 const studentComponent = (data) =>(`
-  <div class="card" style = 'background: #fefefe'>
-    <div class="card-body">
-      <h4 class="card-title">${data.name}</h4>
-      <p class="card-text">${data.direction}</p>
-      <p id ='etiqueta'>${data.averageGrade}</p> 
-      <input type = "hidden" class= "id${data.id}" value = "${data.id}">
-      <button onclick = "deletingStudent('id${data.id}')" id="delete">X</button>
+  <div class="card-body">
+    <h4 class="card-title">${data.name}</h4>
+    <p class="card-text">${data.direction}</p>
+    <p id ='etiqueta'>${data.averageGrade}</p> 
+    <input type = "hidden" class= "id${data.id}" value = "${data.id}">
+    <button onclick = "deletingStudent('id${data.id}')" id="delete">X</button>
 
-      <form method = "GET" action = "/edit"> 
-        <input type = "submit" class = "editar" value = "Editar">
-        <input type = "hidden" name = "id" value = "${data.id}">
-      </form>
-    </div>
+    <form method = "GET" action = "/edit"> 
+      <input type = "submit" class = "editar" value = "Editar">
+      <input type = "hidden" name = "id" value = "${data.id}">
+    </form>
   </div>
 `)
 
 async function renderStudents() {
-  
   const query = GetFetchQueries("student", "getAll")
-  console.log(query);
-  const studentsData = await fetchData(query);
+  // LoaderActivate(students)
+  const { error, data } = await fetchData(query);
 
-  if (studentsData.data.students.length == 0) 
+  if(error) return console.log(error);
+
+  if (data.students.length == 0) 
   {
-
-    console.log("vacio no hay estudiantes, vete pa otra cosa");
-
-  } else {
-
-    console.log(data);
-    let allStudents = []
-
-    studentsData.data.students.forEach(student => allStudents.push(studentComponent(student)) );
-    students.append(allStudents)
-
+    return students.innerHTML = "No se ha creado ningún estudiante en esta escuela."
   }
+
+  let allStudents = []
+  data.students.forEach(student => {
+    const wrapper = document.createElement("div")
+    wrapper.className = "card"
+    wrapper.style.backgroundColor = '#fefefe'
+    wrapper.innerHTML = studentComponent(student)
+
+    allStudents.push(wrapper)
+  });
+
+  students.append(...allStudents)
 }
 
 
---
+++ public/scripts/utils.js
@@ -0,0 +1,8 @@
+function LoaderActivate(Wrapper) {
+    Wrapper.innerHTML = `
+    <div class="loader">
+        <div class="loader-element"></div>
+        <div class="loader-element"></div>
+    </div>
+    `
+}
